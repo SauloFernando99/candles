c95ffeeb3ce21de470ce96318c0b8a8ca283d74c
SauloFernando99 <saulofernando114@gmail.com>
2024-04-15 19:16:33 -0300

    test: new test to verify the validation in the amount of candles sizes

--- src/test/java/CandlesTest.java
+++ src/test/java/CandlesTest.java
@@ -1,9 +1,11 @@
+import org.example.Cake;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CandlesTest {
 
@@ -17,5 +19,12 @@ public class CandlesTest {
         int unLitCandles = cake.blowCandles();
 
         assertEquals(3, unLitCandles);
+    }
+
+    @Test
+    public void testNumberOfCandles() {
+        List<Integer> candlesSizes = Arrays.asList(4, 4, 4, 1, 3);
+
+        assertThrows(IllegalArgumentException.class, () -> new Cake(10, candlesSizes));
     }
 }
